e142c27fa4d97e98895fc284f893cb93bcf0597f
Ian Jenkins <ianparkerjenkins@gmail.com>
2025-12-16 11:28:59 -0800

    update env with pyyaml types; fix pyright for yolo baseline

--- src/skysealand/train/yolo_baseline.py
+++ src/skysealand/train/yolo_baseline.py
@@ -1,13 +1,20 @@
-from ultralytics import YOLO
-import torch
+"""
+A utility script for training a simple, baseline YOLO model on the dataset.
+"""
+
+# I'm pretty sure this works, not sure why pyright is not happy.
 import random
+
 import numpy as np
+import torch
+from ultralytics import YOLO  # pyright: ignore [reportPrivateImportUsage]
+
 
 def train():
     # Load pretrained YOLOv8-nano model
-    model = YOLO("yolov8n.pt")  
+    model = YOLO("yolov8n.pt")
 
-    # Set a fixed seed for reproducability. 
+    # Set a fixed seed for reproducability.
     seed = 42
     torch.manual_seed(seed)
     np.random.seed(seed)
@@ -17,12 +24,12 @@ def train():
     # Train the model
     model.train(
         data="data/data.yaml",  # adjust to your dataset YAML
-        epochs=1,                         # ~20–30 epochs
-        imgsz=640,                         # input size
-        batch=16,                           # adjust for Colab GPU memory
-        workers=2,                          # number of data loader workers
-        seed=42,                            # fixed seed
-        pretrained=True                     # use transfer learning
+        epochs=1,  # ~20–30 epochs
+        imgsz=640,  # input size
+        batch=16,  # adjust for Colab GPU memory
+        workers=2,  # number of data loader workers
+        seed=42,  # fixed seed
+        pretrained=True,  # use transfer learning
     )
 
     # Evaluate on validation set
